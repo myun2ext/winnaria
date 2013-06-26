0a49f52e9795772e1a18dd9f0c834eee1f583f91
myun2 <myun2@nwhite.info>
2013-06-26 18:13:37 +0900

    win32/main.hpp Add

---
+++ include/myun2/win32/main.hpp
@@ -0,0 +1,12 @@
+#ifndef __github_com_myun2__winnaria__main_HPP__
+#define __github_com_myun2__winnaria__main_HPP__
+
+namespace myun2
+{
+	namespace win32
+	{
+		class winmain;
+	}
+}
+
+#endif//__github_com_myun2__winnaria__main_HPP__
