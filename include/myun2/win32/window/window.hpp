788269e34d1cf646949428f2c422562a6eb4a350
myun2 <myun2@nwhite.info>
2013-06-26 18:03:54 +0900

    window class ...

--- include/myun2/win32/window/window.hpp
+++ include/myun2/win32/window/window.hpp
@@ -1,3 +1,12 @@
 #ifndef __github_com_myun2__winoria__window__window_HPP__
 #define __github_com_myun2__winoria__window__window_HPP__
+
+namespace myun2
+{
+	namespace win32
+	{
+		class window;
+	}
+}
+
 #endif//__github_com_myun2__winoria__window__window_HPP__
