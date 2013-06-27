7f36b92671784796b00167ddd641a2cfd2493a2f
myun2 <myun2@nwhite.info>
2013-06-27 12:26:13 +0900

    fix main.hpp and move to /winoria .

---
+++ include/myun2/winoria/main.hpp
@@ -0,0 +1,16 @@
+#ifndef __github_com_myun2__winoria__main_HPP__
+#define __github_com_myun2__winoria__main_HPP__
+
+#include <windows.h>
+
+int WINAPI WinMain(
+	HINSTANCE hInstance, 
+	HINSTANCE hPrevInstance, 
+	LPSTR lpCmdLine, 
+	int nCmdShow
+)
+{
+	
+}
+
+#endif//__github_com_myun2__winoria__main_HPP__
