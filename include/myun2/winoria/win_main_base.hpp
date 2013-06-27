ea89ecbad196dfac4168db312ad3de2c4d4fd156
myun2 <myun2@nwhite.info>
2013-06-27 12:30:03 +0900

    win_main_base.hpp Add

---
+++ include/myun2/winoria/win_main_base.hpp
@@ -0,0 +1,14 @@
+#ifndef __github_com_myun2__winnaria__winmain_holder_class_HPP__
+#define __github_com_myun2__winnaria__winmain_holder_class_HPP__
+
+namespace myun2
+{
+	namespace winoria
+	{
+		class winmain_holder
+		{
+		};
+	}
+}
+
+#endif//__github_com_myun2__winnaria__winmain_holder_class_HPP__
