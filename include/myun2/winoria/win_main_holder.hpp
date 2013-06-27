03a58be4f0da66ed40707975516629fe29515a01
myun2 <myun2@nwhite.info>
2013-06-27 12:40:43 +0900

    rename class: win_main_holder.hpp

---
+++ include/myun2/winoria/win_main_holder.hpp
@@ -0,0 +1,14 @@
+#ifndef __github_com_myun2__winnaria__win_main_holder_HPP__
+#define __github_com_myun2__winnaria__win_main_holder_HPP__
+
+namespace myun2
+{
+	namespace winoria
+	{
+		class win_main_holder
+		{
+		};
+	}
+}
+
+#endif//__github_com_myun2__winnaria__win_main_holder_HPP__
