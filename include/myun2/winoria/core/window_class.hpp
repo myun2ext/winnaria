b2a36f2b766c8cacb9db18a50441e8a0ff228902
myun2 <myun2@nwhite.info>
2013-06-27 17:46:34 +0900

    core/window_class.hpp Add

---
+++ include/myun2/winoria/core/window_class.hpp
@@ -0,0 +1,39 @@
+#ifndef __github_com_myun2__winoria__core__window_class_HPP__
+#define __github_com_myun2__winoria__core__window_class_HPP__
+
+namespace myun2
+{
+	namespace winoria
+	{
+		namespace core
+		{
+			class register_window_class_failed {};
+			class release_window_class_failed {};
+
+			class window_class
+			{
+			protected:
+				WNDCLASS wndcls;
+			public:
+				void reset() { ZeroMemory(wndcls, sizeof(wndcls)); }
+
+				window_class(){ reset(); }
+				window_class(const char* name){
+					reset();
+					wndcls.lpszClassName = name;
+				}
+
+				void regist() const {
+					if ( ::RegisterClass(&wndcls) == 0 )
+						throw register_window_class_failed();
+				}
+				void release() const {
+					if ( ::UnregisterClass(wndcls.lpszClassName) == 0 )
+						throw release_window_class_failed();
+				}
+			};
+		}
+	}
+}
+
+#endif//__github_com_myun2__winoria__core__window_class_HPP__
