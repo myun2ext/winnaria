dd5067d6271feba1ed344ef31a82af9bae83435c
myun2 <myun2@nwhite.info>
2013-07-01 16:20:57 +0900

    core/format_message.hpp Add

---
+++ include/myun2/winoria/core/format_message.hpp
@@ -0,0 +1,26 @@
+#ifndef __github_com_myun2__winoria__core__format_message_HPP__
+#define __github_com_myun2__winoria__core__format_message_HPP__
+
+#include <windows.h>
+
+namespace myun2
+{
+	namespace winoria
+	{
+		namespace core
+		{
+			::std::string format_message(DWORD code)
+			{
+				LPSTR *message_buffer = NULL;
+				if ( ::FormatMessageA(	FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+									NULL, code, LANG_USER_DEFAULT, (LPSTR)&message_buffer, 0, NULL ) == 0 )
+					return 0;
+				::std::string message_str = message_buffer;
+				::LocalFree(message_buffer);
+				return message_str;
+			}
+		}
+	}
+}
+
+#endif//__github_com_myun2__winoria__core__format_message_HPP__
