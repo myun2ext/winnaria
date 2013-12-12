820ba4bbbfd2aa10781e68ac2f710d82395533be
myun2 <myun2@nwhite.info>
2013-12-12 16:14:20 +0900

    winoria basic exception fix.

--- include/myun2/winoria/exception.hpp
+++ include/myun2/winoria/exception.hpp
@@ -10,27 +10,46 @@ namespace myun2
 		class exception : public ::std::string
 		{
 		public:
-			int code;
-			exception() {}
-			exception(const char* s): ::std::string(s) {}
-			exception(const ::std::string& s): ::std::string(s) {}
-			exception(int code_in): code(code_in) {}
-			exception(int code_in, const char* s): code(code_in), ::std::string(s) {}
-			exception(int code_in, const ::std::string& s): code(code_in), ::std::string(s) {}
+			const int code;
+			const ::std::string name;
+			typedef ::std::string message;
+
+			exception(const char* name_in)
+				: name(name_in), code(0) {}
+			exception(const char* name_in, int code_in)
+				: name(name_in), code(code_in) {}
+
+			exception(const char* name_in, const char* msg_in)
+				: name(name_in), message(msg_in) {}
+			exception(const char* name_in, const ::std::string& msg_in)
+				: name(name_in), message(msg_in) {}
+
+			exception(const char* name_in, int code_in, const char* msg_in)
+				: name(name_in), code(code_in), message(msg_in) {}
+			exception(const char* name_in, int code_in, const ::std::string& msg_in)
+				: name(name_in), code(code_in), message(msg_in) {}
 		};
 	}
 }
 
-#define DECL_WINORIA_EXCEPTION(name)	\
-		class name : public exception	\
+#define DECL_WINORIA_EXCEPTION(ClassName)	\
+		class ClassName : public ::myun2::winoria::exception	\
 		{	\
+		private:	\
+			typedef ::myun2::winoria::exception _Base;	\
 		public:	\
-			name() {}	\
-			name(const char* s): exception(s) {}	\
-			name(const ::std::string& s): exception(s) {}	\
-			name(int code_in): exception(code_in) {}	\
-			name(int code_in, const char* s): exception(code_in, s) {}	\
-			name(int code_in, const ::std::string& s): exception(code_in, s) {}	\
+			ClassName(const char* name_in)	\
+				: _Base(name_in) {}	\
+			ClassName(const char* name_in, int code_in)	\
+				: _Base(name_in, code_in) {}	\
+			ClassName(const char* name_in, const char* msg_in)	\
+				: _Base(name_in, msg_in) {}	\
+			ClassName(const char* name_in, const ::std::string& msg_in)	\
+				: _Base(name_in, msg_in) {}	\
+			ClassName(const char* name_in, int code_in, const char* msg_in)	\
+				: _Base(name_in, code_in, msg_in) {}	\
+			ClassName(const char* name_in, int code_in, const ::std::string& msg_in)	\
+				: _Base(name_in, code_in, msg_in) {}	\
 		};
 
 #endif//__github_com_myun2__winoria__exception_HPP__
