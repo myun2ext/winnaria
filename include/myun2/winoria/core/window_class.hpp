0f25710af44625a54a01749b46d10c533526bc9d
myun2 <myun2@nwhite.info>
2013-12-13 19:10:04 +0900

    window_class.hpp update

--- include/myun2/winoria/core/window_class.hpp
+++ include/myun2/winoria/core/window_class.hpp
@@ -1,6 +1,8 @@
 #ifndef __github_com_myun2__winoria__core__window_class_HPP__
 #define __github_com_myun2__winoria__core__window_class_HPP__
 
+#include "myun2/winoria/exception.hpp"
+#include <string>
 #include <windows.h>
 
 namespace myun2
@@ -9,20 +11,21 @@ namespace myun2
 	{
 		namespace core
 		{
-			class register_window_class_failed {};
-			class release_window_class_failed {};
+			DECL_WINORIA_EXCEPTION( register_window_class_failed );
+			DECL_WINORIA_EXCEPTION( release_window_class_failed );
 
 			class window_class
 			{
-			protected:
-				WNDCLASS wndcls;
 			public:
-				void reset() { ZeroMemory(&wndcls, sizeof(wndcls)); }
-
-				window_class(){ reset(); }
-				window_class(const char* name){
-					reset();
-					wndcls.lpszClassName = name;
+				::WNDCLASS wndcls;
+			private:
+				::std::string window_class_name;
+				void zeroreset() { ZeroMemory(&wndcls, sizeof(wndcls)); }
+			public:
+				window_class(){ zeroreset(); }
+				window_class(const char* name) : window_class_name(name) {
+					zeroreset();
+					wndcls.lpszClassName = window_class_name.c_str();
 				}
 
 				void regist() const {
@@ -33,6 +36,11 @@ namespace myun2
 					if ( ::UnregisterClass(wndcls.lpszClassName, hAppInstance) == 0 )
 						throw release_window_class_failed();
 				}
+
+				::WNDCLASS& get_wndclass() { return wndcls; }
+				const ::WNDCLASS& get_wndclass() const { return wndcls; }
+
+				const char* get_class_name() const { return wndcls.lpszClassName; }
 			};
 		}
 	}
