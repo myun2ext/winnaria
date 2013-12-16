d1efe5618f31c2bfa4583a6aa80bf2f1675e7cd6
myun2 <myun2@nwhite.info>
2013-12-16 19:27:22 +0900

    core/atom.hpp Add

---
+++ include/myun2/winoria/core/atom.hpp
@@ -0,0 +1,28 @@
+#ifndef __github_com_myun2__winoria__core__atom_HPP__
+#define __github_com_myun2__winoria__core__atom_HPP__
+
+#include <windows.h>
+
+namespace myun2
+{
+	namespace winoria
+	{
+		namespace core
+		{
+			class atom
+			{
+			public:
+				typedef ATOM ImplType, atom_type;
+			private:
+				atom_type value;
+			public:
+				atom(){}
+				atom(const ImplType& in_value) : value(in_value) {}
+			};
+		}
+	}
+}
+
+#endif//__github_com_myun2__winoria__core__atom_HPP__
+//http://msdn.microsoft.com/en-us/library/windows/desktop/ms649053(v=vs.85).aspx
+//http://msdn.microsoft.com/ja-jp/library/cc429245.aspx
