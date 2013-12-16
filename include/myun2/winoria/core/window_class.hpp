c5055c902961ef665b1add451ae267bef466dd0d
myun2 <myun2@nwhite.info>
2013-12-16 17:37:40 +0900

    minimum...

--- include/myun2/winoria/core/window_class.hpp
+++ include/myun2/winoria/core/window_class.hpp
@@ -11,46 +11,8 @@ namespace myun2
 	{
 		namespace core
 		{
-			DECL_WINORIA_EXCEPTION( register_window_class_failed );
-			DECL_WINORIA_EXCEPTION( release_window_class_failed );
-
-			////
-
 			class window_class
 			{
-			private:
-				typedef WNDCLASSEX core_type;
-				typedef ATOM atom_type;
-
-				core_type wndcls;
-				atom_type atom;
-				::std::string window_class_name;
-
-				void reset_class() {
-					ZeroMemory(&wndcls, sizeof(wndcls));
-					wndcls.cbSize = sizeof(core_type);
-				}
-			public:
-				window_class() { reset_class(); }
-				window_class(const char* name) : window_class_name(name) {
-					reset_class();
-					wndcls.lpszClassName = window_class_name.c_str();
-				}
-
-				void regist() const {
-					atom = ::RegisterClassEx(&wndcls)
-					if ( atom == 0 )
-						throw register_window_class_failed();
-				}
-				void release(HINSTANCE hAppInstance = NULL) const {
-					if ( ::UnregisterClass(get_class_name(), hAppInstance) == 0 )
-						throw release_window_class_failed();
-				}
-
-				core_type& get_wndclass() { return wndcls; }
-				const core_type& get_wndclass() const { return wndcls; }
-
-				const char* get_class_name() const { return window_class_name.c_str(); }
 			};
 		}
 	}
---
+++ window_class_test.cpp
@@ -0,0 +1,10 @@
+#include "myun2/winoria/core/window_class.hpp"
+#include <stdio.h>
+
+using myun2::winoria::core::window_class;
+
+int main()
+{
+//	window_class wc("Test");
+//	wc.wndcls. 
+}
