c1bf0db101f84486d8c8e4a1a035afefe72c77a2
myun2 <myun2@nwhite.info>
2013-12-16 20:58:45 +0900

    atom: allocate() Add

--- include/myun2/winoria/core/atom.hpp
+++ include/myun2/winoria/core/atom.hpp
@@ -1,6 +1,7 @@
 #ifndef __github_com_myun2__winoria__core__atom_HPP__
 #define __github_com_myun2__winoria__core__atom_HPP__
 
+#include "myun2/winoria/exception.hpp"
 #include <windows.h>
 
 namespace myun2
@@ -9,6 +10,8 @@ namespace myun2
 	{
 		namespace core
 		{
+			DECL_WINORIA_EXCEPTION( failed_add_atom );
+
 			class atom
 			{
 			public:
@@ -17,9 +20,17 @@ namespace myun2
 				atom_type value;
 			public:
 				atom(){}
+				atom(LPCTSTR name){ allocate(name); }
 				atom(const ImplType& in_value) : value(in_value) {}
 
 				operator atom_type() const { return value; }
+
+				static ATOM allocate(LPCTSTR name) {
+					ATOM ret = ::AddAtom(name);
+					if ( ret == 0 )
+						throw failed_add_atom();
+					return ret;
+				}
 			};
 		}
 	}
